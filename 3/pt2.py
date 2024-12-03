4b9dcbff556a5eda8bf0dbc74618cc2553c5d087
rocco gibbons <roccogibbons@outlook.com>
2024-12-03 17:10:38 +0000

    finished day 3

---
+++ 3/pt2.py
@@ -0,0 +1,74 @@
+with open("input.txt", "r") as f:
+    lines = f.readlines()
+
+total = 0
+skip = False
+
+for line in lines:
+    for i in range(len(line)):
+        if line[i] == "d":
+            condition = ""
+            condition += line[i] + line[i+1] + line[i+2] + line[i+3] + line[i+4] + line[i+5] + line[i+6]
+            if condition == "don't()":
+                skip = True
+            elif condition[:4]:
+                skip = False
+
+
+        if line[i] == "m":
+            if skip == True:
+                continue
+            equation = ""
+            equation += line[i] + line[i+1] + line[i+2] + line[i+3]
+            if equation != "mul(":
+                continue
+
+            equation += line[i+4] + line[i+5] + line[i+6] + line[i+7] + line[i+8] + line[i+9] + line[i+10] + line[i+11]
+            new_eq = ""
+            valid = False
+            #check for end bracket and cut off string after it
+            for i in range(len(equation)):
+                if equation[i] == ")":
+                    new_eq += equation[i]
+                    valid = True
+                    break
+                new_eq += equation[i]
+
+            if valid == False:
+                continue
+
+            #check interior of string
+            valid = True
+            has_comma = False
+            for c in new_eq:
+                if c not in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "m", "u", "l", "(", ")", ","]:
+                    valid = False #continue here and set valid var
+                    break
+                if c == ",":
+                    has_comma = True
+
+            if valid == False:
+                continue
+            if has_comma == False:
+                continue
+
+            num1 = ""
+            num2 = ""
+            num_all = new_eq[4:]
+            num_all = num_all[:-1]
+            
+            past = False
+            for c in num_all:            
+                if c == ",":
+                    past = True
+                    continue
+                if past == False:
+                    num1 += c
+                elif past == True:
+                    num2 += c
+            
+            total += int(num1) * int(num2)
+
+print(total)
+            
+        
